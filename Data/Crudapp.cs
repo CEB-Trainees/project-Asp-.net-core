9c5ff5d98d8ff45f72225f41a04a1839b1f9fe52
sulaksha-git <sulakshanirman97@gmail.com>
2020-10-24 10:18:13 +0530

    data folder

---
+++ Data/Crudapp.cs
@@ -0,0 +1,18 @@
+﻿using Crudapp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crudapp.Data
+{
+    public class CrudappContext : DbContext
+    {
+        public CrudappContext (DbContextOptions<CrudappContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
